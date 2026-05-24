d89ba191589af9f5fd93c174e62bd1cca13b9ed0
jgomezz <jgomezz@gmail.com>
2026-05-24 12:10:55 -0500

    update

--- src/main/java/pe/edu/tecsup/restful/CpeJavaRestfulApplication.java
+++ src/main/java/pe/edu/tecsup/restful/CpeJavaRestfulApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CpeJavaMod03S04RestfulApplication {
+public class CpeJavaRestfulApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CpeJavaMod03S04RestfulApplication.class, args);
+		SpringApplication.run(CpeJavaRestfulApplication.class, args);
 	}
 
 }
