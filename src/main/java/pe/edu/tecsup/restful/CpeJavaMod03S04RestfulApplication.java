b0f92553640bd441d39bae42bf5976af5ec0ad51
jgomezz <jgomezz@gmail.com>
2026-05-24 12:05:40 -0500

    first version

---
+++ src/main/java/pe/edu/tecsup/restful/CpeJavaMod03S04RestfulApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.tecsup.restful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CpeJavaMod03S04RestfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CpeJavaMod03S04RestfulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/tecsup/restful/CpeJavaMod03S04RestfulApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.tecsup.restful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CpeJavaMod03S04RestfulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
